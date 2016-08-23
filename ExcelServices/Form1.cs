6a53c5517cf5a83d1db1d28bbc3fc7f0d3aa058f
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 14:55:50 +0200

    moved to GitHub

---
+++ ExcelServices/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelServices.EuroStrategy;
+//using System.Web.Services.Protocols;
+
+namespace ExcelServices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExcelServiceSoapClient es = new ExcelServiceSoapClient();
+            Status[] outStatus;
+            RangeCoordinates rangeCoordinates = new RangeCoordinates();
+            string sheetName = "Hoja1";
+            string targetWorkbookPath = "http://eurostrategy.sharepoint.com/Documentos%20compartidos/Ejemplo%20Excel%20externo%2010%20-%20oData%20feed%20PLSQL.xlsx";
+            //es..Credentials = System.Net.CredentialCache.DefaultCredentials;
+                string sessionId = es.OpenWorkbook(targetWorkbookPath, "en-US", "en-US", out outStatus);
+                es.CloseWorkbook(sessionId);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"Repository "ExcelServices" moved to Github, © Davide Moraschi 2016 davidem@eurostrategy.net or [visit my web page](http://moraschi.com)" 
